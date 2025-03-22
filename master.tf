22babffc6e4aa4adabdb1f5b901d3219e08053ce
matheusfrancisco <matheusmachadoufsc@gmail.com>
2025-03-22 15:47:41 -0300

    add: upgrade to recent version

--- master.tf
+++ master.tf
@@ -1,47 +1,49 @@
 resource "proxmox_vm_qemu" "masters" {
 
     count = var.master_nr
-    
-    # VM General Settings
     target_node = var.target_node
-    vmid = var.master_id_range + count.index + 1
+
     name = format("%s-${count.index + 1}", var.master_naming)
+    vmid = var.master_id_range + count.index + 1
 
-    # VM Advanced General Settings
+
+    clone = "debian-11-cloudinit-template"
     onboot = true 
+    agent = 1 
+    os_type = "cloud-init"
+    cpu = "host"
 
-    # VM OS Settings
-    clone = "ubuntu-2204-cloud-init"
-
-    # VM System Settings
-    agent = 1
-    
-    # VM CPU Settings
     cores = var.master_cores
     sockets = var.master_sockets
-    
-    # VM Memory Settings
     memory = var.master_memory
-
-    # Cloud-Init
-    # Needed for correct state of Terraform
     ciuser = var.ciuser
+    
     sshkeys = var.ssh_keys
     ipconfig0 = "ip=${cidrhost(var.bridge_cidr_range, var.master_network_range + count.index)}/24,gw=${cidrhost(var.bridge_cidr_range, 1)}"
 
-    # VM Network Settings
     network {
-        id = count.index + 1
         bridge = var.bridge_network
         model  = "virtio"
     }
 
-    disk {
-      type = "disk"
-      slot = "scsi0"
-      storage = "local-lvm"
-      size = var.master_disksize
+    disks {
+      ide {
+          ide2 {
+              cloudinit {
+                 storage = "local-lvm"
+              }
+          }
+      }
+      scsi {
+         scsi0 {
+            disk {
+               size = var.master_disksize
+               storage = "local-lvm"
+            }
+         }
+      }
     }
+    boot = "order=scsi0;ide2"
 
     tags = "masters"
 
--- worker.tf
+++ worker.tf
@@ -1,47 +1,48 @@
 resource "proxmox_vm_qemu" "workers" {
-
     count = var.worker_nr
-    
-    # VM General Settings
     target_node = var.target_node
-    vmid = var.worker_id_range + count.index + 1
+
     name = format("%s-${count.index + 1}", var.worker_naming)
+    vmid = var.worker_id_range + count.index + 1
 
-    # VM Advanced General Settings
+
+    clone = "debian-11-cloudinit-template"
     onboot = true 
+    agent = 1 
+    os_type = "cloud-init"
+    cpu = "host"
 
-    # VM OS Settings
-    clone = "ubuntu-2204-cloud-init"
-
-    # VM System Settings
-    agent = 1
-    
-    # VM CPU Settings
     cores = var.worker_cores
     sockets = var.worker_sockets
-
-    # VM Memory Settings
     memory = var.worker_memory
-
-    # Cloud-Init
     ciuser = var.ciuser
+    
     sshkeys = var.ssh_keys
     ipconfig0 = "ip=${cidrhost(var.bridge_cidr_range, var.worker_network_range + count.index)}/24,gw=${cidrhost(var.bridge_cidr_range, 1)}"
 
-    # VM Network Settings
     network {
-      id = count.index + 1
-      bridge = var.bridge_network
-      model  = "virtio"
-      firewall = true
+        bridge = var.bridge_network
+        model  = "virtio"
     }
 
-    disk {
-      type = "disk"
-      slot = "scsi0"
-      storage = "local-lvm"
-      size = var.worker_disksize
+    disks {
+      ide {
+          ide2 {
+              cloudinit {
+                 storage = "local-lvm"
+              }
+          }
+      }
+      scsi {
+         scsi0 {
+            disk {
+               size = var.worker_disksize
+               storage = "local-lvm"
+            }
+         }
+      }
     }
+    boot = "order=scsi0;ide2"
 
     tags = "workers"
 
